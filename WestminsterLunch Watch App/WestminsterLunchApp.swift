c8febd7991ba4e1e2fadedb68f870c8bf4e29091
Connor Christopherson <connorchristopherson@westminster.net>
2026-08-17 20:29:53 -0400

    Initial Commit

---
+++ WestminsterLunch Watch App/WestminsterLunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WestminsterLunchApp.swift
+//  WestminsterLunch Watch App
+//
+//  Created by Connor Christopherson on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct WestminsterLunch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
